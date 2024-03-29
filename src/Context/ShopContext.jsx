4430ccd007c45b6016fc5450d265ba8399091c18
Aral Hudaoglu <aralhudaoglu@Arals-MacBook-Air.local>
2024-03-29 15:31:26 +0000

    finalised front-end project

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -22,18 +22,28 @@ const ShopContextProvider = (props) => {
   };
   const getTotalCartAmount = () => {
     let totalAmount = 0;
-    for (const item in cartItems) 
-    {
-      if (cartItems[item] > 0) 
-      {
-        let itemInfo = all_product.find((product)=>product.id === Number(item))
-        totalAmount = totalAmount+ (itemInfo.new_price * cartItems[item]);
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount = totalAmount + itemInfo.new_price * cartItems[item];
       }
-    
     }
     return totalAmount;
   };
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
   const contextValue = {
+    getTotalCartItems,
     getTotalCartAmount,
     all_product,
     cartItems,
